c8fd421217fd9df28973c6267c9d98f2b39cddb4
cryptojunkie <mrcholo21@icloud.com>
2022-10-27 17:58:13 +0100

    api returns json

--- src/index.js
+++ src/index.js
@@ -15,6 +15,4 @@ app.get('/', (req, res) => {
     res.status(200).json({slackUsername: 'offorifeanayor', backend: true, age: 26, Bio: "I am a passionate, driven and ambitious software engineer. I enjoy reading, travelling and playing with my sons. Yeah I am also married with two lovely boys."})
 })
 
-app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
-})
+app.listen(PORT)
