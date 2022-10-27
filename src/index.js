5982c18de74e45044d3855ef64fbc28b5ac7849a
cryptojunkie <mrcholo21@icloud.com>
2022-10-27 13:10:24 +0100

    api returns json

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
