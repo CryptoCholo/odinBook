5a7423526a58031e742cd56b3f2b1b85c15b82b4
cryptojunkie <mrcholo21@icloud.com>
2022-10-27 13:05:46 +0100

    hng task1

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 
 app.get('/', (req, res) => {
-    res.json({slackUsername: 'MrCholo', backend: true, age: 26, Bio: "Passionate driven ambitious"})
+    res.json({slackUsername: 'Mr Cholo', backend: true, age: 26, Bio: "I am a passionate, driven and ambitious software engineer. I enjoy reading, "})
 })
 
 app.listen(PORT, () => {
