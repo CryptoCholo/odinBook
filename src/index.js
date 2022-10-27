e6dcde62e9d722fd293055b8948e617acc5b123c
cryptojunkie <mrcholo21@icloud.com>
2022-10-27 19:53:27 +0100

    added cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
