527f145c7447069d670fa362924d8cef8fcd1c44
cryptojunkie <mrcholo21@icloud.com>
2022-10-28 14:50:57 +0100

    updated json response

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 4000;
 
 
 app.use(cors({
-    origin: "*"
+    origin: '*'
 }));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
    
-    res.status(200).json({slackUsername: 'offorifeanayor', backend: true, age: 26, Bio: "I am a passionate, driven and ambitious software engineer. I enjoy reading, travelling and playing with my sons. Yeah I am also married with two lovely boys."})
+    res.status(200).json({slackUsername:  'offorifeanayor', backend:  true, age:  26, bio:  "I am a passionate, driven and ambitious software engineer. I enjoy reading, travelling and playing with my sons."})
 })
 
 app.listen(PORT)
