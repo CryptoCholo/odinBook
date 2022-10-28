580600f366c8331d6b87b5dc16f5150150681509
cryptojunkie <mrcholo21@icloud.com>
2022-10-28 16:40:03 +0100

    updated json response

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
    
-    res.status(200).json({slackUsername:  'offorifeanayor', backend:  true, age:  26, bio:  "I am a passionate, driven and ambitious software engineer. I enjoy reading, travelling and playing with my sons."})
+    res.status(200).json({slackUsername:  'MrCholo', backend:  true, age:  26, bio:  "I am a passionate, driven and ambitious software engineer. I enjoy reading, travelling and playing with my sons."})
 })
 
 app.listen(PORT)
