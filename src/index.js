8ffbb445e0b9ed751770d3ace6466d14911e927a
cryptojunkie <mrcholo21@icloud.com>
2022-10-28 08:41:53 +0100

    cors issue resolved

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
